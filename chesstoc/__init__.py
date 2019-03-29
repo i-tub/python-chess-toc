900f813bf0430662fbe715399267e565e1bad61c
Ivan Tubert-Brohman <Ivan.Tubert-Brohman@schrodinger.com>
2019-03-29 19:58:31 -0400

    First real commit
    Added Python sources, templates, data and examples.

---
+++ chesstoc/__init__.py
@@ -0,0 +1,160 @@
+"""
+Functions to analyze a game an plot the resulting evaluation on top of a
+diagram of the final position of the game.
+"""
+
+__author__ = "Ivan Tubert-Brohman"
+__email__ = "ivan.tubert@gmail.com"
+__version__ = "0.1"
+
+import itertools
+import json
+import os
+
+import chess
+import chess.engine
+import chess.pgn
+import chess.svg
+
+import matplotlib.pyplot as plt
+
+from svgutils import compose
+
+# Used to convert mating positions to centipawns
+MAX_SCORE = 10000
+
+# Figure size in points
+SIZE = 360
+
+# Margins as fraction of the figure area which will be covered by the axes
+LEFT_BOTTOM_MARGIN = 0.05
+TOP_RIGHT_MARGIN = 0.98
+
+
+def analyze_game(game, engine, time_per_move=1.0, verbose=False):
+    """
+    Analyze a game using an engine and return the numerical evaluation of
+    each position.
+
+    :param game: game to analyze
+    :type game: chess.pgn.Game
+
+    :param game: engine to use for analysis
+    :type game: chess.engine.SimpleEngine
+
+    :param time_per_move: time to spend on each full move (2 plies), in seconds
+    :type time_per_move: float
+
+    :return: the evaluation for every position in the game's mainline,
+        in full points from White's point of view.
+    :rtype: list of float
+    """
+    limit = chess.engine.Limit(time=time_per_move / 2)
+
+    scores = []
+    for ply, node in enumerate(itertools.chain([game], game.mainline())):
+        board = node.board()
+        result = engine.play(board, limit, info=chess.engine.INFO_SCORE)
+        score = result.info['score']
+        besteval = score.white().score(mate_score=MAX_SCORE) / 100
+        if verbose:
+            print(ply, besteval, (ply + 1) // 2, node.san() if ply else '')
+        scores.append(besteval)
+    return scores
+
+
+def classify_opening(game):
+    """
+    :param game: game to analyze
+    :type game: chess.pgn.Game
+
+    :return: ECO code and opening name
+    :rtype: tuple (str, str)
+    """
+    ecofile = os.path.join(os.path.dirname(__file__), 'eco/eco.json')
+    ecodata = json.load(open(ecofile, 'r'))
+    openings = {o['f']: o for o in ecodata}
+
+    for node in reversed(game.mainline()):
+        eco_fen = ' '.join(node.board().fen().split()[:3])
+        opening = openings.get(eco_fen)
+        if opening:
+            return opening['c'], opening['n']
+    return None, None
+
+
+def write_plot(scores, filename, scale=8.0):
+    """
+    Write a plot of the scores to `filename`. The Y axis ranges from -scale to
+    +scale, and the X axis is the full move number from 0 to the end of the
+    game.
+
+    :param scores: evaluation for every position in the game's mainline,
+            in full points from White's point of view.
+    :type scores: list of float
+
+    :param filename: file to write
+    :type filename: str
+
+    :param scale: limit of Y axis
+    :type scale: float
+    """
+    # shift margin a bit to make sure line is still visible when capped
+    cap = scale - 0.05
+    y = [min(max(s, -cap), cap) for s in scores]
+    x = [0.5 * i for i in range(len(y))]
+
+    size_in_inches = SIZE / 72.0
+    fig, ax = plt.subplots(figsize=(size_in_inches, size_in_inches))
+
+    ax.set_ylim(-scale, scale)
+    ax.set_xlim(0, max(x))
+    ax.plot(x, y, color='black')
+
+    # Leave only a minimal amount of padding (e.g. for the axis labels).
+    fig.subplots_adjust(LEFT_BOTTOM_MARGIN, LEFT_BOTTOM_MARGIN,
+                        TOP_RIGHT_MARGIN, TOP_RIGHT_MARGIN)
+
+    fig.savefig(filename, transparent=True)
+    plt.close(fig)
+
+
+def write_board(game, filename, size=SIZE):
+    """
+    Write a diagram of the final position of the game to an SVG file.
+
+    :param game: game to create the figure for
+    :type game: chess.pgn.Game
+
+    :param filename: file to write
+    :type filename: str
+
+    :param size: size of the figure, passed to chess.svg.board().
+    :type size: int
+    """
+    with open(filename, mode='w') as fh:
+        fh.write(
+            chess.svg.board(game.end().board(), size=size, coordinates=False))
+
+
+def compose_svg(svg_board, svg_plot, svg_combined):
+    """
+    Create a combined SVG in which the board image is put in the background of
+    the axes area of the plot image.
+
+    :param svg_board: filename of existing board image.
+    :type svg_board: str
+
+    :param svg_plot: filename of existing plot image.
+    :type svg_plot: str
+
+    :param svg_combined: filename of combined image, to be written.
+    :type svg_combined: str
+    """
+    scale = TOP_RIGHT_MARGIN - LEFT_BOTTOM_MARGIN
+    xdel = LEFT_BOTTOM_MARGIN * SIZE
+    ydel = (1.0 - TOP_RIGHT_MARGIN) * SIZE
+    compose.Figure(
+        SIZE, SIZE,
+        compose.Panel(compose.SVG(svg_board).scale(scale).move(xdel, ydel)),
+        compose.Panel(compose.SVG(svg_plot))).save(svg_combined)
---
+++ chesstoc/__main__.py
@@ -0,0 +1,206 @@
+#!/usr/bin/env python3
+"""
+Take a PGN file and generate an HTML file with a table of contents for the PGN
+file. Each entry in the table is a board with the final position for a game,
+superimposed with the plot of the engine evaluation function.
+"""
+
+import argparse
+import os
+import sys
+
+import chess.engine
+import chess.pgn
+import jinja2
+
+import chesstoc
+
+
+def parse_args():
+    """
+    Parse the command-line arguments.
+
+    :rtype: argparse.Namespace
+    """
+    parser = argparse.ArgumentParser(prog='annotator', description=__doc__)
+    parser.add_argument(
+        "pgn_file", help="input PGN file", metavar="<filename>")
+    parser.add_argument(
+        "--engine",
+        "-e",
+        metavar='<engine>',
+        help="analysis engine (default: %(default)s)",
+        default="stockfish")
+    time_group = parser.add_mutually_exclusive_group()
+    time_group.add_argument(
+        "--time_per_move",
+        "-t",
+        help="how long to spend on each move "
+        "(default: %(default)s)",
+        default=1.0,
+        type=float,
+        metavar="<seconds>")
+    parser.add_argument(
+        "--threads",
+        help="threads for use by the engine "
+        "(default: %(default)s)",
+        metavar='<int>',
+        type=int,
+        default=4)
+    parser.add_argument(
+        "--maxgames",
+        "-m",
+        help="max number of games to evaluate. ",
+        metavar='<int>',
+        type=int)
+    parser.add_argument(
+        "--columns",
+        help="number of games per row (default: %(default)s)",
+        metavar='<int>',
+        default=4,
+        type=int)
+    parser.add_argument(
+        "--html", help="HTML output file with table of contents")
+    parser.add_argument(
+        "--template", metavar='<filename>', help="template file to use")
+    parser.add_argument(
+        "--css", metavar='<filename>', help="stylesheet to use")
+    parser.add_argument(
+        "-T",
+        dest='templatepath',
+        metavar='<directory>',
+        action='append',
+        help="directory to search for templates (may be used multiple times)")
+    parser.add_argument(
+        "--dryrun",
+        action='store_true',
+        help="skip the game analysis and plotting")
+
+    return parser.parse_args()
+
+
+def checkgame(game):
+    """
+    Check for PGN parsing errors and raise a ValueError if any were found.
+
+    :param game: game to check
+    :type game: chess.pgn.Game
+    """
+    if game.errors:
+        msg = "There were errors parsing the PGN game:\n"
+        for error in game.errors:
+            msg += error + '\n'
+        raise RuntimeError(msg)
+
+    # Try to verify that the PGN file was readable
+    if game.end().parent is None:
+        raise RuntimeError(
+            "Could not render the board. Is the file legal PGN?")
+
+
+def pgn_reader(pgn_file):
+    """
+    A generator that yields the games from a PGN file as well as a dict with
+    basic metadata. The dict contains the PGN headers, plus "WhiteResult" and
+    "BlackResult" for convenience, as well as an "index" which is the position
+    of the game in the file, counting from 1.
+
+    :param pgn_file: input filename
+    :type pgn_file: str
+
+    :return: generator of games and metadata
+    :rtype: generator of tuples (chess.pgn.Game, dict)
+    """
+    with open(pgn_file) as pgn:
+        for index, game in enumerate(
+                iter(lambda: chess.pgn.read_game(pgn), None), 1):
+            try:
+                checkgame(game)
+            except ValueError as e:
+                sys.exit(e)
+            metadata = {'index': index}
+            metadata.update(game.headers)
+            try:
+                results = metadata['Result'].split('-')
+                # Fake PGN headers with each color result for convenience
+                metadata['WhiteResult'], metadata['BlackResult'] = results
+            except:
+                pass
+            yield game, metadata
+
+
+def get_css():
+    """
+    Return the body of the default css file.
+
+    :rtype: str
+    """
+    css = os.path.join(os.path.dirname(__file__), 'templates', 'chesstoc.css')
+    with open(css) as fh:
+        return fh.read()
+
+
+def write_html(games, args):
+    """
+    Write an HTML file with a table of contents for a collection of games.
+
+    :param games: game metadata, where each game is a dict with PGN headers
+                  as well as the `svg_combined` filename.
+    :type games: iterable of dict
+
+    :param args: command-line arguments
+    :type args: argparse.Namespace
+    """
+    if args.template:
+        loader = jinja2.FileSystemLoader(args.templatepath or '.')
+    else:
+        loader = jinja2.PackageLoader('chesstoc')
+    env = jinja2.Environment(loader=loader)
+    template = env.get_template('template.html')
+    with open(args.html, 'w') as fh:
+        fh.write(
+            template.render({
+                'games': games,
+                'args': args,
+                'get_css': get_css
+            }))
+
+
+def main():
+    args = parse_args()
+
+    games = []
+    for game, g in pgn_reader(args.pgn_file):
+        print("Game %d: %s: %s - %s" % (g['index'], g.get('Event'),
+                                        g.get('White'), g.get('Black')))
+        basename = "%03d" % g['index']
+        svg_plot = f'{basename}-plot.svg'
+        svg_board = f'{basename}-board.svg'
+        svg_combined = f'{basename}.svg'
+
+        g['ECO'], g['Opening'] = chesstoc.classify_opening(game)
+        g['svg_plot'] = svg_plot
+        g['svg_board'] = svg_board
+        g['svg_combined'] = svg_combined
+        games.append(g)
+
+        chesstoc.write_board(game, svg_board)
+
+        if not args.dryrun:
+            with chess.engine.SimpleEngine.popen_uci(args.engine) as engine:
+                engine.configure({"Threads": args.threads})
+                scores = chesstoc.analyze_game(game, engine,
+                                               args.time_per_move)
+
+            chesstoc.write_plot(scores, svg_plot)
+            chesstoc.compose_svg(svg_board, svg_plot, svg_combined)
+
+        if args.maxgames is not None and g['index'] == args.maxgames:
+            break
+
+    if args.html:
+        write_html(games, args)
+
+
+if __name__ == "__main__":
+    main()
